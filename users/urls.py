a25e5cdd2e4a5a1fb344370c066245c3bf5c638c
antzw <wen_study@outlook.com>
2023-06-11 17:20:19 +0800

    Add register function.

--- users/urls.py
+++ users/urls.py
@@ -2,8 +2,13 @@
 
 from django.urls import path, include
 
+from . import views
+
 app_name = 'users'
 urlpatterns = [
     # 包含默认的身份验证URL。
     path('',include('django.contrib.auth.urls')),
+    # 包含注册页面
+    path('register/', views.register, name='register'),
+
 ]
