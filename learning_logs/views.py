f04b2dc2f37d5eada870ad788a7e1307c9944e53
antzw <wen_study@outlook.com>
2023-06-11 19:58:10 +0800

    Add Edit enrty function.

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -17,6 +17,7 @@ def topics(request):
     context={'topics':topics}
     return render(request, 'learning_logs/topics.html', context)
 
+@login_required
 def topic(request, topic_id):
     """显示单个主题及其所有的条目"""
     topic=Topic.objects.get(id=topic_id)
@@ -25,7 +26,7 @@ def topic(request, topic_id):
     return render(request,'learning_logs/topic.html',context)
 
 
-
+@login_required
 def new_topic(request):
     """添加新主题"""
     if request.method != 'POST':
@@ -42,7 +43,7 @@ def new_topic(request):
     context = {'form': form}
     return render(request,'learning_logs/new_topic.html',context)
 
-
+@login_required
 def new_entry(request, topic_id):
     """在特定主题中添加新条目"""
     topic = Topic.objects.get(id=topic_id)
@@ -63,4 +64,21 @@ def new_entry(request, topic_id):
     context ={'topic':topic, 'form':form}
     return render(request, 'learning_logs/new_entry.html',context)
     
+def edit_entry(request, entry_id):
+    """编辑既有条目"""
+    entry = Entry.objects.get(id=entry_id)
+    topic = entry.topic
+
+    if request.method != 'POST':
+        # 初次请求：使用当前条目填充表单
+        form=EntryForm(instance=entry)
+    else:
+        # POST提交的数据：对数据进行处理
+        form=EntryForm(instance=entry, data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('learning_logs:topic', topic_id=topic.id)
+
+    context = {'entry':entry, 'topic':topic, 'form':form}
+    return render(request, 'learning_logs/edit_entry.html', context)
 
